08efd35ba6102e75372dee5188b0ff59e90543b8
Alexander (Rouse_) Bagel <rouse79@yandex.ru>
2025-12-31 09:45:35 +0600

    #2363 License change for FWHexView.Cairo

--- src/FWHexView.Cairo.pas
+++ src/FWHexView.Cairo.pas
@@ -16,25 +16,7 @@
 //
 
 {
-Licence:
-  FWHexView is dual-licensed. You may choose to use it under the restrictions of the GPL v3 licence at no cost to you,
-  or you may purchase a commercial licence. A commercial licence grants you the right to use FWHexView in your own
-  applications, royalty free, and without any requirement to disclose your source code nor any modifications to FWHexView
-  to any other party. A commercial licence lasts into perpetuity, and entitles you to all future updates, free of
-  charge. A commercial licence is sold per developer developing applications that use FWHexView, as follows:
-    1 developer = $49
-    2 developers = $89
-    3 developers = $139
-    4 developers = $169
-    5 developers = $199
-    >5 developers = $199 + $25 per developer from the 6th onwards
-    site licence = $499 (unlimited number of developers affiliated with the owner of the licence, i.e. employees, co-workers, interns and contractors)
-
-  Please send an e-mail to hexview_sale@rousehome.ru to request an invoice before or after payment is made. Payment may be
-  made via bank transfer. Bank details will be provided on the invoice.
-
-  Support (via e-mail) is available for users with a commercial licence. Enhancement requests submitted by users with a
-  commercial licence will be prioritized.
+License: MPL 2.0 or LGPL
 }
 
 unit FWHexView.Cairo;
