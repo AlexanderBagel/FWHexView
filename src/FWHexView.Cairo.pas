7230227577df087568e51a92edaf8f957bd363fe
Alexander (Rouse_) Bagel <rouse79@yandex.ru>
2026-01-09 23:09:03 +0600

    #2384 Optimizing text output via Cairo

--- src/FWHexView.Cairo.pas
+++ src/FWHexView.Cairo.pas
@@ -32,9 +32,14 @@ uses
   SysUtils,
   Graphics;
 
-  function CairoDrawText(ACanvas: TCanvas; Str: string;
+  function CairoDrawText(ACanvas: TCanvas; const Str: string;
     var ARect: TRect; Flags: Cardinal): Integer;
   function CairoExtTextOut(ACanvas: TCanvas; X, Y: Integer; Options: Longint;
+    ARect: PRect; Str: PChar; Count: Longint; Dx: PInteger): Boolean;
+
+  function CairoDrawTextOld(ACanvas: TCanvas; const Str: string;
+    var ARect: TRect; Flags: Cardinal): Integer;
+  function CairoExtTextOutOld(ACanvas: TCanvas; X, Y: Integer; Options: Longint;
     ARect: PRect; Str: PChar; Count: Longint; Dx: PInteger): Boolean;
 
 implementation
@@ -43,7 +48,13 @@ uses
   gdk2,
   Gtk2Def,
   Cairo,
+  pango,
+  pangocairo,
+  glib2,
   Math;
+
+var
+  CairoColors: array [0..255] of Double;
 
 type
   ECairoException = class(Exception);
@@ -108,9 +119,9 @@ end;
 function cairo_get_color(AColor: TColor): TCairoColor;
 begin
   AColor := ColorToRGB(AColor);
-  Result.R := GetRValue(AColor) / 255;
-  Result.G := GetGValue(AColor) / 255;
-  Result.B := GetBValue(AColor) / 255;
+  Result.R := CairoColors[GetRValue(AColor)];
+  Result.G := CairoColors[GetGValue(AColor)];
+  Result.B := CairoColors[GetBValue(AColor)];
   Result.A := 1.0;
 end;
 
@@ -127,7 +138,260 @@ begin
   Result := Ceil(extents.height - extents.descent);
 end;
 
-function CairoDrawText(ACanvas: TCanvas; Str: string;
+type
+  TCairoContext = record
+    Context: pcairo_t;
+    Font: Pcairo_scaled_font_t;
+    Size: Integer;
+    Glyphs: Pcairo_glyph_t;
+    GlyphsLen: LongInt;
+    Clusters: PCairoClusterArray;
+    ClustersLen: LongInt;
+    InvalidGlyphsPresent: Boolean;
+  end;
+
+procedure ReleaseCairoContext(var AContext: TCairoContext);
+begin
+  cairo_glyph_free(AContext.Glyphs);
+  cairo_text_cluster_free(pcairo_text_cluster_t(AContext.Clusters));
+  cairo_destroy(AContext.Context);
+end;
+
+function CreateCairoContext(ACanvas: TCanvas; Str: PChar;
+  Count, X, Y: Integer; out AContext: TCairoContext): Boolean;
+var
+  cluster_flags: cairo_text_cluster_flags_t;
+  glyphs: Pcairo_glyph_t;
+  I, A: Integer;
+begin
+  AContext := Default(TCairoContext);
+  AContext.Context := cairo_create_context(ACanvas.Handle);
+  cairo_set_font(AContext.Context, ACanvas.Font);
+  cairo_set_source_color(AContext.Context, cairo_get_color(ACanvas.Font.Color));
+  AContext.Font := cairo_get_scaled_font(AContext.Context);
+  AContext.Size := ACanvas.Font.Size;
+  Result := cairo_scaled_font_text_to_glyphs(AContext.Font,
+    X, Y + cairo_font_baseline(AContext.Font),
+    Str, Count, @AContext.Glyphs, @AContext.GlyphsLen, @AContext.Clusters,
+    @AContext.ClustersLen, @cluster_flags) = CAIRO_STATUS_SUCCESS;
+  if not Result then
+  begin
+    ReleaseCairoContext(AContext);
+    Exit;
+  end;
+  glyphs := AContext.Glyphs;
+  for I := 0 to AContext.ClustersLen - 1 do
+    for A := 0 to AContext.Clusters^[I].num_glyphs - 1 do
+    begin
+      if glyphs^.index = 0 then
+      begin
+        AContext.InvalidGlyphsPresent := True;
+        Break;
+      end;
+      Inc(glyphs);
+    end;
+end;
+
+procedure DrawPangoGlyph(const ct: TCairoContext; Str: PChar; X: Integer;
+  CalcRect: PRect; Dx: PInteger);
+var
+  layout: PPangoLayout;
+  font_descriptions: PPangoFontDescription;
+  glyphs, start: Pcairo_glyph_t;
+  I, A, Len, BytesLen, GlyphOffset: Integer;
+  CurrentValid, GlyphValid: Boolean;
+
+  procedure DrawPart;
+  var
+    pX, pY: LongInt;
+    CurrentGlyphX: Double;
+  begin
+    if Dx = nil then
+      CurrentGlyphX := glyphs^.x
+    else
+      CurrentGlyphX := X;
+    if CalcRect = nil then
+    begin
+      if start^.index > 0 then
+        cairo_glyph_path(ct.Context, start, Len)
+      else
+      begin
+        cairo_move_to(ct.Context, start^.x, start^.y);
+        pango_layout_set_text(layout, Str, BytesLen);
+        pango_layout_get_pixel_size(layout, @pX, @pY);
+        pango_cairo_show_layout_line(ct.Context, pango_layout_get_line(layout, 0));
+        GlyphOffset := pX - Ceil(CurrentGlyphX - start^.x);
+      end;
+    end
+    else
+      if start^.index = 0 then
+      begin
+        pango_layout_set_text(layout, Str, BytesLen);
+        pango_layout_get_pixel_size(layout, @pX, @pY);
+        CalcRect^.Height := pY;
+        Inc(CalcRect^.Right, pX - Ceil(CurrentGlyphX - start^.x) + 1);
+      end;
+    start := glyphs;
+    Inc(Str, BytesLen);
+    BytesLen := 0;
+    Len := 0;
+  end;
+
+begin
+  layout := pango_cairo_create_layout(ct.Context);
+  font_descriptions := pango_font_description_new;
+  pango_font_description_set_size(font_descriptions, PANGO_SCALE * ct.Size);
+  pango_layout_set_font_description(layout, font_descriptions);
+  pango_font_description_free(font_descriptions);
+
+  glyphs := ct.Glyphs;
+  start := glyphs;
+  BytesLen := 0;
+  Len := 0;
+  GlyphOffset := 0;
+  CurrentValid := glyphs^.index > 0;
+  for I := 0 to ct.ClustersLen - 1 do
+  begin
+    for A := 0 to ct.Clusters^[I].num_glyphs - 1 do
+    begin
+      GlyphValid := glyphs^.index > 0;
+      if GlyphValid <> CurrentValid then
+      begin
+        DrawPart;
+        CurrentValid := GlyphValid;
+      end;
+      if Dx <> nil then
+      begin
+        glyphs^.x := X;
+        Inc(X, Dx^);
+        Inc(Dx);
+      end;
+      glyphs^.x := glyphs^.x + GlyphOffset;
+      Inc(BytesLen, ct.Clusters^[I].num_bytes);
+      Inc(glyphs);
+    end;
+    Inc(Len);
+  end;
+
+  DrawPart;
+  if CalcRect = nil then
+    cairo_fill_preserve(ct.Context);
+
+  g_object_unref(layout);
+end;
+
+function CairoDrawText(ACanvas: TCanvas; const Str: string; var ARect: TRect;
+  Flags: Cardinal): Integer;
+var
+  ct: TCairoContext;
+  textents: cairo_text_extents_t;
+  x, y, awidth: Integer;
+begin
+  Result := 0;
+  if not CreateCairoContext(ACanvas, PChar(Str), -1, ARect.Left, ARect.Top, ct) then Exit;
+  try
+    cairo_scaled_font_extents(ct.Font, @textents);
+    Result := Ceil(textents.height);
+
+    cairo_text_extents(ct.Context, PChar(Str), @textents);
+    if Flags and DT_CALCRECT <> 0 then
+    begin
+      ARect.Width := Ceil(textents.width);
+      ARect.Height := Max(Result, Ceil(textents.height));
+      if ct.InvalidGlyphsPresent then
+        DrawPangoGlyph(ct, PChar(Str), ARect.Left, @ARect, nil);
+      Exit;
+    end;
+
+    if Flags and DT_NOCLIP = 0 then
+    begin
+      cairo_rectangle(ct.Context, ARect.Left, ARect.Top, ARect.Width + 1, ARect.Height);
+      cairo_clip(ct.Context);
+    end;
+
+    x := ARect.Left;
+    y := ARect.Top + cairo_font_baseline(ct.Font);
+    awidth := Ceil(textents.width);
+    if Flags and DT_CENTER <> 0 then
+      x := ARect.Left + (ARect.Width - awidth) div 2;
+    if Flags and DT_RIGHT <> 0 then
+      x := ARect.Right - awidth;
+
+    if ACanvas.Brush.Style = bsSolid then
+    begin
+      ARect.Left := Max(X, ARect.Left);
+      ACanvas.FillRect(ARect);
+    end;
+
+    if ct.InvalidGlyphsPresent then
+      DrawPangoGlyph(ct, PChar(Str), x, nil, nil)
+    else
+    begin
+      cairo_move_to(ct.Context, x, y);
+      cairo_show_text(ct.Context, PChar(Str));
+    end;
+
+  finally
+    ReleaseCairoContext(ct);
+  end;
+end;
+
+function CairoExtTextOut(ACanvas: TCanvas; X, Y: Integer; Options: Longint;
+  ARect: PRect; Str: PChar; Count: Longint; Dx: PInteger): Boolean;
+var
+  ct: TCairoContext;
+  glyphs: Pcairo_glyph_t;
+  I, A: Integer;
+begin
+  Result := CreateCairoContext(ACanvas, Str, Count, X, Y, ct);
+  if not Result then Exit;
+  try
+    if ARect <> nil then
+    begin
+      cairo_rectangle(ct.Context, ARect^.Left, ARect^.Top, ARect^.Width + 1, ARect^.Height);
+      if ACanvas.Brush.Style = bsSolid then
+      begin
+        cairo_save(ct.Context);
+        cairo_set_source_color(ct.Context, cairo_get_color(ACanvas.Brush.Color));
+        cairo_fill(ct.Context);
+        cairo_restore(ct.Context);
+      end;
+      if Options and ETO_CLIPPED <> 0 then
+        cairo_clip(ct.Context)
+    end;
+
+    if ct.InvalidGlyphsPresent then
+    begin
+      DrawPangoGlyph(ct, PChar(Str), X, nil, Dx);
+      Exit;
+    end;
+
+    if Dx = nil then
+    begin
+      cairo_move_to(ct.Context, x, y + cairo_font_baseline(ct.Font));
+      cairo_show_text(ct.Context, Str);
+      Exit;
+    end;
+
+    glyphs := ct.Glyphs;
+    for I := 0 to ct.ClustersLen - 1 do
+      for A := 0 to ct.Clusters^[I].num_glyphs - 1 do
+      begin
+        glyphs^.x := X;
+        Inc(X, Dx^);
+        Inc(glyphs);
+        Inc(Dx);
+      end;
+
+    cairo_glyph_path(ct.Context, ct.Glyphs, ct.GlyphsLen);
+    cairo_fill_preserve(ct.Context);
+
+  finally
+    ReleaseCairoContext(ct);
+  end;
+end;
+
+function CairoDrawTextOld(ACanvas: TCanvas; const Str: string;
   var ARect: TRect; Flags: Cardinal): Integer;
 var
   ct: pcairo_t;
@@ -175,7 +439,7 @@ begin
   end;
 end;
 
-function CairoExtTextOut(ACanvas: TCanvas; X, Y: Integer; Options: Longint;
+function CairoExtTextOutOld(ACanvas: TCanvas; X, Y: Integer; Options: Longint;
   ARect: PRect; Str: PChar; Count: Longint; Dx: PInteger): Boolean;
 var
   ct: pcairo_t;
@@ -186,6 +450,7 @@ var
   num_clusters: LongInt;
   cluster_flags: cairo_text_cluster_flags_t;
   I, A: Integer;
+  R: TRect;
 begin
   ct := cairo_create_context(ACanvas.Handle);
   try
@@ -229,6 +494,14 @@ begin
     cairo_destroy(ct);
   end;
 end;
+
+var
+  I: Integer;
+
+initialization
+
+  for I := 0 to 255 do
+    CairoColors[I] := I / 255;
 
 end.
 
